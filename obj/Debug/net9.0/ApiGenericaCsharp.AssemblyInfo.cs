4b9c8b5b28bf5e55f75de302612425dcb3cffdf5
Jheinson G <jjhamid123@gmail.com>
2026-02-12 16:45:54 -0500

    Instalar paquetes NuGet y crear interfaz IProveedorConexion

--- obj/Debug/net9.0/ApiGenericaCsharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiGenericaCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e98550cf4adce8a55b7e08b8234593a2cdae9655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d6ca503e7220bbd6010b37eb440b5f9883b3d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
